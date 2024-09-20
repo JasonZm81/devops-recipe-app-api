e7e39de6212870863d919866449ae28b1a977f2b
JasonZm81 <zhengming8198@gmail.com>
2024-09-20 21:14:36 +0800

    added terraform

---
+++ infra/deploy/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket               = "devops-recipe-app-tf-state-bucket-jason-demo"
+    key                  = "tf-state-deploy"
+    workspace_key_prefix = "tf-state-deploy"
+    region               = "us-east-1"
+    encrypt              = true
+    dynamodb_table       = "devops-recipe-app-api-tf-lock-jason-demo"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManagedBy   = "Terraform/deploy"
+    }
+  }
+}
+
+# dynamic string 
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+}
+
+data "aws_region" "current" {}
