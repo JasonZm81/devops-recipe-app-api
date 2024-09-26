3fc185721a6cea63c9a4bbce8015459754e53d06
JasonZm81 <zhengming8198@gmail.com>
2024-09-27 01:39:07 +0800

    setup github action workflows

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -9,7 +9,9 @@ resource "aws_iam_access_key" "cd" {
   user = aws_iam_user.cd.name
 }
 
+###########################################################################
 # Policy for Terraform backend to S3 and Dynamo DB access
+###########################################################################
 data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect    = "Allow"
@@ -52,4 +54,42 @@ resource "aws_iam_policy" "tf_backend" {
 resource "aws_iam_user_policy_attachment" "tf_backend" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_backend.arn
+}
+
+#########################
+# Policy for ECR access #
+#########################
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.app.arn,
+      aws_ecr_repository.proxy.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.cd.name}-ecr"
+  description = "Allow user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
+
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecr.arn
 }
