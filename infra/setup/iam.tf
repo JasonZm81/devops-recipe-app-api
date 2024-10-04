6e5343f795db5e15da75292f18ea525d3b5c770d
JasonZm81 <zhengming8198@gmail.com>
2024-10-04 17:17:17 +0800

    added security group for ecs - fixing rds iam

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -168,7 +168,6 @@ data "aws_iam_policy_document" "rds" {
       "rds:DescribeDBSubnetGroups",
       "rds:DescribeDBInstances",
       "rds:CreateDBSubnetGroup",
-      "rds:ModifyDBSubnetGroup", 
       "rds:DeleteDBSubnetGroup",
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
